34c35fc6475661ee36d68bc96fba058bf9ad6e15
Mieeeee <1392445927@qq.com>
2022-11-23 16:50:31 +0800

    first

---
+++ IParams1.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public interface IParams {
+    public int getBig();
+    public int getMedium();
+    public int getSmall();
+    public void set(int big,int medium,int small);
+    public ArrayList<Integer> getPlanParking();
+    public void add(int i);
+}
